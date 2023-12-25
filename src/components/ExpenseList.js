31a395fc628e253d29b9cee5f7ef1b47d551339f
Benson Mwangi <kimaniben@gmail.com>
2023-12-25 15:41:00 -0500

    Change of $ to Ksh

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -8,11 +8,11 @@ const ExpenseList = ({ expenses, onDeleteExpense }) => {
 
   return (
     <>
-      <h2>Total Expenses: ${totalExpenses.toFixed(2)}</h2>
+      <h2>Total Expenses: Ksh {totalExpenses.toFixed(2)}</h2>
       <ul>
         {expenses.map((expense, index) => (
           <li key={index}>
-            {expense.description} - ${expense.amount} ({expense.category}) - {expense.date}
+            {expense.description} - Ksh {expense.amount} ({expense.category}) - {expense.date}
             <button onClick={() => onDeleteExpense(index)}>Delete</button>
           </li>
         ))}
